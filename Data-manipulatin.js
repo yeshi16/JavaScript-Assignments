22b4a9cfc6f3b4f62f679a7f2db0528c79fbbef4
m-s1621 <mebashimelis@gmail.com>
2024-04-19 23:50:54 -0400

    Data manipulation

---
+++ Data-manipulatin.js
@@ -0,0 +1,32 @@
+// Create variable declarations using both let and const.
+let var1 = 52;
+const var2 = "JavaScript";
+
+// Apply different types of data literals.
+console.log(true);
+
+// Use arithmetic operators to manipulate data.
+let operation = 5 - 3;
+
+// Use comparison operators to compare data.
+let num1 = 6;
+let num2 = 9;
+if(num1 > num2){
+    console.log(num1);
+}
+    
+// Perform string concatenations.
+console.log("Hello" + "WISE");
+
+// Implement escape sequences in strings for special characters.
+console.log("Hello" + "\"WISE\"");
+
+// Use template literals for string interpolation and multi-line strings.
+const str = `
+Hello,
+WISE!
+`
+console.log(str);
+
+// Create effective documentation through the use of comments.
+// all of the questions are in comment for clarification of each question for the answers given bellow it.
