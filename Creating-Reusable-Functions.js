42336557b9c08a22ddc23d3aad9119dba948c26e
m-s1621 <mebashimelis@gmail.com>
2024-04-23 15:30:10 -0400

    Creating-Reusable-Functionspart-1

---
+++ Creating-Reusable-Functions.js
@@ -0,0 +1,13 @@
+// Part 1: Thinking Functionally
+
+// Take an array of numbers and return the sum.
+
+const arr =[ 2,6,8,7,9,16];
+let total = 0;
+function sum(arr){
+    for(let num in arr){
+      return total += num;  
+    }
+}
+
+
